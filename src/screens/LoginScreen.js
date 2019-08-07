e456a19e3b431f3ac90f855cfb85ef889ba65fff
black9 <csu02049@naver.com>
2019-08-07 18:04:52 +0900

    Today Study Upload Finish :)
    Todo. 로그인 에러 수정

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -9,21 +9,47 @@ import MyButton from '../components/MyButton'
 export default class LoginScreen extends React.Component{
     constructor(props){
         super(props)
+        const LogId = this.props.navigation.getParam('LogId')
         this.state ={
-            name: '',
-            email: '',
+            userid: '',
+            pwd: '',
+            LogId: LogId
         }
+        if (loginId !== null)
+        return this.props.navigation.navigate('MyCarList',{
+          loginId:loginId
+        }
+        )
     }
+    submitSignup = async () => {
+        const userid = this.state.userid;
+        const pwd = this.state.pwd;
+
+        let user = AsyncStorage.getItem(`user:$(userid)`);
+        if (user !==null){
+            alert("이미 가입된 아이디입니다.")
+            return false
+        }
+
+        AsyncStorage.setItem(`user:${userid}`, pwd);
+
+        return this.props.navigation.navigate('MyCarList',{
+            loginId:loginId
+        })
+
+        return true;
+    } 
     
     render(){
         return(
             <View>
             <Text style={{textAlign: 'center', fontSize:24, }}>CAR AUCTION</Text>
-            <TextInput style={styles.textinput} placeholder="이름" onChangeText={(text) => this.setState({ name: text })} />
-            <TextInput style={styles.textinput} placeholder="이메일" onChangeText={(text) => this.setState({ email: text })} />
-            <MyButton text="로그인"iconName="ios-calendar"> </MyButton>
 
-            <MyButton text="회원가입"iconName="ios-calendar"> </MyButton>
+            <TextInput style={styles.textinput} placeholder="이름" onChangeText={(text) => this.setState({ userid: text })}  />
+            <TextInput style={styles.textinput} placeholder="비밀번호" onChangeText={(text) => this.setState({ pwd: text })} />
+            <MyButton text="로그인"iconName="ios-arrow-round-forward" onPress={this.submitSignup}> </MyButton>
+
+            <MyButton text="회원가입"iconName="ios-walk" onPress> </MyButton>
             </View>
 
         )
